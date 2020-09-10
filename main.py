a4364f05ff6353b9046a7c67009f4caff6ca8322
Rachael Graveling <clrg092@cl.glow.scot>
2020-09-10 10:38:18 +0000

    Working

--- main.py
+++ main.py
@@ -4,38 +4,49 @@ SBB Task 4a - Student Record
 Rachael Graveling
 09/09/2020
 
-A brief description of what the program does
+Cities (Lists of Objects)
+
+Reads in the data for a record and stores in a object
+TODO: Add the record to a list so that we have a list of objects
 """
 
 class Record():
-    def __init__(self, name, age, height, town):
-        self.name = name
-        self.age = age
-        self.height = height
-        self.town = town
+    def __init__(self, city, country, population, language):
+        self.city = city
+        self.country = country
+        self.population = population
+        self.language = language
 
 def main():
-    student = get_record()
-    display_record(student)
+    city = get_record()
+    display_record(city)
 
 def get_record():
-    name = input('Please enter name: ')
-    age = int(input('Please enter age: '))
-    while age < 1 or age > 150 or not age.is_integer():
-        print('Error. Must be a whole number between 1 & 150')
-        age = float(input('Please enter age : '))
+    # TODO: A list for storing the individual records (lists)
+    list = []
 
-    height = float(input('Please enter height: '))
-    while height < 1 or height > 2.5:
-        print('Error. Must be a number between 1 & 2.5')
-        height = float(input('Please enter height : '))
-    town = input('What is your home town?: ')
+    # Total number of records
+    no_of_cities = 3
 
-    return Record(name, age, height, town)
+    # TODO: Loop for number of record entries
+    for x in range (no_of_cities):
+    # Input data for record
+      city = input('Please enter a city > ')
+      country = input('Please enter country > ')
+      population = float(input('Please enter the population (millions) > '))
+      language = input('Please enter language > ')
+      print()
 
-def display_record(record):
-    print()
-    print(record.name, record.age, record.height, record.town)
+    # TODO: Add record to list
+      list.append(Record(city, country, population, language))
+
+
+    # TODO: Return list instead of single record
+    return list
+
+def display_record(list):
+    # TODO: Replace this with printing all records
+    for record in list:
+      print(record.city, record.country, record.population, record.language)
 
 main()
-
